e47c73e14c27f88ffadcf0336cbf3db425dc11eb
Ahmad Raza <razarajpoot99598@gmail.com>
2026-04-14 23:05:34 +0500

    Resolved merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
-
 import './App.css'
 import NavBar from './components/NavBar'
 
 function App() {
-
-
   return (
     <>
-    <NavBar />
-     <h1 className='text-3xl font-bold'>Getting started with tailwind and DaisyUI</h1>
-=======
-import './App.css'
-
-function App() {
-
-  return (
-    <>
-      <h1>Let's get started with DevConnect</h1>
->>>>>>> 72271c011680b0e74d54781f058acb1da992ddbc
+      <NavBar />
+      <h1 className='text-3xl font-bold'>
+        Getting started with tailwind and DaisyUI
+      </h1>
     </>
   )
 }
