dbe855bb833242b0a169457c245148d8e0c1deaa
Ahmad Raza <razarajpoot99598@gmail.com>
2026-04-14 23:11:36 +0500

    installed daisyUI

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-<div className="max-lg:collapse bg-base-200 lg:mb-48 shadow-sm w-full rounded-md">
-  
-  <div className="collapse-title navbar justify-between">
-    <div className="navbar-start">
-      <button className="btn btn-ghost text-xl">DesiGhee Scene</button>
-    </div>
+    <div className="max-lg:collapse bg-base-200 lg:mb-48 shadow-sm w-full rounded-md">
+    
+        <div className="collapse-title navbar justify-between">
+            <div className="navbar-start">
+            <button className="btn btn-ghost text-xl">DesiGhee Scene</button>
+            </div>
 
-    <div className="navbar-center  lg:flex">
-      <ul className="menu menu-horizontal px-1">
-        <li><button>Item 1</button></li>
-        <li><button>Item 3</button></li>
-      </ul>
-    </div>
-   
-  </div>
+            <div className="navbar-center  lg:flex">
+            <ul className="menu menu-horizontal px-1">
+                <li><button>Item 1</button></li>
+                <li><button>Item 3</button></li>
+            </ul>
+            </div>
+        
+        </div>
 
-</div>
+    </div>
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
-=======
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> 72271c011680b0e74d54781f058acb1da992ddbc
+  plugins: [react(), tailwindcss()],
 })
