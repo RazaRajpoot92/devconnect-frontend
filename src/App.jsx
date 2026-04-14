7b69a271d28b67dfd9f0bd69580a4e261ef455de
Ahmad Raza <razarajpoot99598@gmail.com>
2026-04-14 22:58:17 +0500

    frontend started

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import NavBar from './components/NavBar'
+
+function App() {
+
+
+  return (
+    <>
+    <NavBar />
+     <h1 className='text-3xl font-bold'>Getting started with tailwind and DaisyUI</h1>
+    </>
+  )
+}
+
+export default App
