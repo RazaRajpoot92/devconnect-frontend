0807cb4edb9507ebd24e40b79e93486b24eb956b
Ahmad Raza <razarajpoot99598@gmail.com>
2026-04-17 21:32:21 +0500

    basic app ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import './App.css'
 import NavBar from './components/NavBar'
+import Body from './components/Body'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <>
-      <NavBar />
-      <h1 className='text-3xl font-bold'>
-        Getting started with tailwind and DaisyUI
-      </h1>
+     <BrowserRouter basename='/'>
+        <Routes>
+            <Route path='/' element={<Body />}>
+                <Route path='/' element={<HomePage />}  />
+                <Route path='/login' element={<Login />}  />
+                <Route path='/signup' element={<SignUp />}  />
+            </Route>
+        </Routes>
+     </BrowserRouter>
+     
     </>
   )
 }
